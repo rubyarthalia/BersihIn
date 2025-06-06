8a954ab6b6e0b1b15034e212c014600252ef2568
Ruby <arthaliaruby@gmail.com>
2025-06-06 11:49:15 +0700

    Fix: Admin features

---
+++ app/Models/Order.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $primaryKey = 'id';
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'id', 'customer_id', 'tanggal_jadwal', 'ongkos_kirim', 'harga_total',
+        'metode_pembayaran', 'status_pembayaran', 'status_layanan', 'status_del',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
+    public function orderServices()
+    {
+        return $this->hasMany(OrderService::class, 'order_id', 'id');
+    }
+
+}
---
+++ app/Models/OrderService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderService extends Model
+{
+    protected $table = 'order_services';
+
+    protected $fillable = [
+        'order_id', 'service_id', 'cleaner_id', 'jumlah', 'catatan', 'sub_total', 'status_del'
+    ];
+    
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'order_id', 'id');
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class, 'service_id', 'id');
+    }
+}
